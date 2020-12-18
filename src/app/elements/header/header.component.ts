7ffc3942900b463c3c10f2024778d1806f3ea281
AllanA <allana@cloudstaff.com>
2020-12-18 10:26:53 +0800

    added logout button

--- src/app/elements/header/header.component.ts
+++ src/app/elements/header/header.component.ts
@@ -1,15 +1,35 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnChanges, OnInit, SimpleChanges } from '@angular/core';
+import { GlobalService } from '../../services/global.service';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.css']
 })
-export class HeaderComponent implements OnInit {
+export class HeaderComponent implements OnInit, OnChanges {
 
-  constructor() { }
+  isLogged: boolean;
 
-  ngOnInit(): void {
+  constructor(private globalService: GlobalService) { 
+    this.isLogged = false;
   }
 
+  ngOnChanges(changes: SimpleChanges): void {
+    throw new Error('Method not implemented.');
+  }
+
+  ngOnInit(): void {
+    // console.log('header-onInit:');
+    this.isLogged = this.globalService.isAuthenticated();
+    
+    this.globalService.isLogged.subscribe(
+      (logged: boolean) => {
+        this.isLogged = logged;
+      }
+    );
+  }
+
+  onLogout(): void {    
+    this.globalService.logout();
+  }
 }
