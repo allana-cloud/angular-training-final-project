1b3a03a2ad3f1a53c1626f92fd8b8bae3c274a8a
AllanA <allana@cloudstaff.com>
2020-12-18 10:27:15 +0800

    added the sidebase routine

--- src/app/elements/sidebar/sidebar.component.ts
+++ src/app/elements/sidebar/sidebar.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { GlobalService } from '../../services/global.service';
+import { UserProfile } from '../../models/profile.model';
 
 @Component({
   selector: 'app-sidebar',
@@ -6,10 +8,44 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./sidebar.component.css']
 })
 export class SidebarComponent implements OnInit {
+  
+  userProfile: UserProfile = {
+    name: '',
+    jobTitle: '',
+    email: '',
+    mobileNumber: ''
+  };
 
-  constructor() { }
+  constructor(private globalService: GlobalService) { }
 
   ngOnInit(): void {
+    // console.log('sidebar-OnInit');
+    if (this.globalService.isAuthenticated()) {      
+      this.userProfile = this.globalService.getUserProfileData();        
+    }
+    
+    this.globalService.profileData.subscribe(
+      (hasUpdate: boolean) => {
+        if (hasUpdate) {
+          // console.log('sidebar-event-profileData');
+          this.userProfile = this.globalService.getUserProfileData();
+        }
+      }
+    );
+
+    this.globalService.isLogged.subscribe(
+      (logged: boolean) => {   
+        if (!logged) {
+          // console.log('has logged out clear user data now');
+          this.userProfile = {
+            name: '',
+            jobTitle: '',
+            email: '',
+            mobileNumber: ''
+          }          
+        }    
+      }
+    );
   }
 
 }
