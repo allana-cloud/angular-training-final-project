dc75aee4d20c7e6faaa79920015ee6b2569e81fb
AllanA <allana@cloudstaff.com>
2020-12-18 10:26:32 +0800

    added logout button routine

--- src/app/elements/footer/footer.component.ts
+++ src/app/elements/footer/footer.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { GlobalService } from '../../services/global.service';
 
 @Component({
   selector: 'app-footer',
@@ -7,11 +8,21 @@ import { Component, OnInit } from '@angular/core';
 })
 export class FooterComponent implements OnInit {
 
+  isLogged: boolean;
+
   currentDate = Date.now();
 
-  constructor() { }
+  constructor(private globalService: GlobalService) { }
 
   ngOnInit(): void {
+    // console.log('header-onInit:');
+    this.isLogged = this.globalService.isAuthenticated();
+
+    this.globalService.isLogged.subscribe(
+      (logged: boolean) => {
+        this.isLogged = logged;
+      }
+    );
   }
 
 }
