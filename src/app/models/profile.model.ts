03b25f489bf60db1ef5fb91c0e302100152646c4
AllanA <allana@cloudstaff.com>
2020-12-18 12:23:06 +0800

    Add Reactive form

--- src/app/models/profile.model.ts
+++ src/app/models/profile.model.ts
@@ -2,5 +2,11 @@ export interface UserProfile {
     name: string,
     jobTitle: string,
     email: string,
-    mobileNumber: string
+    mobileNumber: string,
+    firstName: string,
+    lastName: string,
+    photoUrl: string,
+    alias: string
 }
+
+// default value for photo url https://via.placeholder.com/150
--- src/app/pages/profile/profile.component.ts
+++ src/app/pages/profile/profile.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormControl } from '@angular/forms';
 import { AppComponent } from '../../app.component';
+import { GlobalService } from '../../services/global.service';
+import { UserProfile } from '../../models/profile.model';
 
 @Component({
   selector: 'app-profile',
@@ -8,10 +11,35 @@ import { AppComponent } from '../../app.component';
 })
 export class ProfileComponent implements OnInit {
 
-  constructor(private appComponent: AppComponent) { }
+  profileForm : FormGroup;
+
+  constructor(
+    private appComponent: AppComponent,
+    private globalService: GlobalService
+  ) { }
 
   ngOnInit(): void {    
     this.appComponent.setTitle('Profile');
+
+    this.profileForm = new FormGroup({      
+      firstName: new FormControl(''),
+      lastName: new FormControl(''),
+      alias: new FormControl(''),
+      jobTitle: new FormControl(''),
+      email: new FormControl(''),
+      mobileNumber: new FormControl(''),
+    });
+
+    // console.log('data',this.globalService.getUserProfileData());
+    this.fillForm(this.globalService.getUserProfileData());    
+  }
+
+  onSubmit(): void {
+    console.log('onsubmit');
+  }
+
+  fillForm(userProfile: UserProfile): void {    
+    this.profileForm.patchValue(userProfile);
   }
 
 }
